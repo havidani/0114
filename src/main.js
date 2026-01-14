b9b66b5ac01135962ed354799c369e3de049259f
Petrik Diak <diak@petrik.hu>
2026-01-14 12:17:32 +0100

    js kész 3. és 2. lépés kész minden kész

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const grrrr = function (e) {
+  document.getElementById("yfdojnb").reset();
+  document.getElementById("uzenet").textContent = 'Sikeres regisztráció';
+  e.preventDefault();
+};
+ 
+document.getElementById('gomb').addEventListener('click', grrrr);
